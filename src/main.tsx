315885a5fdbbaf86644b80026165c507c1ae9102
jesus <jgarridocruz5@gmail.com>
2026-04-19 18:30:17 -0600

    Se agregÃo del cdn de bulma junto con una plantilla para su uso

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      {/*link para importar los iconos de font awesome (solo los free)*/}
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
+            integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
+            crossOrigin="anonymous" referrerPolicy="no-referrer"/>
     <App />
   </StrictMode>,
 )
